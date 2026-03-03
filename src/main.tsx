efaee3dc4a76f147f807ce1757ad4e0e79b2abcb
Geoffrey Dobson <30755162+gbdobson@users.noreply.github.com>
2026-03-03 10:47:24 -0500

    Set up React app with AWS Amplify configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import '@aws-amplify/ui-react/styles.css';
+import { Amplify } from 'aws-amplify';
+import outputs from '../amplify_outputs.json';
 
+Amplify.configure(outputs);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
