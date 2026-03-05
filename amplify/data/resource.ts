1dd0518243f04b4f094745644798f78566239483
Geoffrey Dobson <30755162+gbdobson@users.noreply.github.com>
2026-03-05 14:06:52 -0500

    Make ageGroup, gender, and role fields required

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -3,15 +3,15 @@ import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 const schema = a.schema({
   Team: a.model({
     name: a.string().required(),
-    ageGroup: 'U10 | U11 | U12 | U13 | U14 | U18',
-    gender: 'boys | girls',
+    ageGroup: a.string().required(),
+    gender: a.string().required(),
     seasonWeek: a.integer().required(),
     seasonNotes: a.string(),
   }).authorization((allow) => [allow.owner()]),
   
   Message: a.model({
     team: a.belongsTo('Team', 'teamId'),
-    role: 'coach | bot',
+    role: a.string().required(),
     content: a.string().required(),
     timestamp: a.datetime().required(),
   }).authorization((allow) => [allow.owner()]),
